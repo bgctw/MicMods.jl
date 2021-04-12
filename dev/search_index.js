39d466f0fb7d0f6f95461cf52b18e24832d711ac
Documenter.jl <documenter@juliadocs.github.io>
2021-04-12 12:04:54 +0000

    build based on 8034bd6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MicMods","category":"page"},{"location":"#MicMods","page":"Home","title":"MicMods","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MicMods.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MicMods]","category":"page"}]
+}
